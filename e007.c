efbb30b25f68b12a50c9f1ca7444c3ade588e7f5
Lopes Carlos <soudeve@gmail.com>
2024-10-01 02:07:56 +0100

    Add files via upload

---
+++ e007.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+	int a,b;
+	printf("Working whit bit the bit in c\n");
+	a = 10;
+	b = a>>1;
+	printf("a: %d\tb: %d\n",a,b);
+	//bin(a,b);
+	return 0;
+}
