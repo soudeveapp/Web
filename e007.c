e80df961c35055d625ec1093124c0c27cd846d53
LopesCarlos <soudeve@gmail.com>
2024-10-01 21:32:17 +0100

    Testar enviar Lopes Carlos

--- e007.c
+++ e007.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+//ex 07
 int main(){
 	int a,b;
 	printf("Working whit bit the bit in c\n");
-	a = 10;
-	b = a>>2;
+	a = 1;
+	b = a<<2;//igual a 4
 	printf("a: %d\tb: %d\n",a,b);
 	//bin(a,b);
 	return 0;
